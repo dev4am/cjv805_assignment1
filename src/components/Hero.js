1646ff310ecacf94e05b2dbb714133027a98b43d
zfkhrl@gmail.com <zfkhrl@gmail.com>
2021-07-09 15:16:53 -0400

    slides

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,46 +1,83 @@
 import React from 'react'
-import {Carousel} from "react-bootstrap";
+import {Card, Carousel} from "react-bootstrap";
 
 function Hero() {
+
+    const carousel = {
+        color: "white",
+        width: "100%",
+        height: "400px",
+        // backgroundColor: 'rgba(38,179,170)',
+        backgroundImage: 'linear-gradient(to right, rgba(38,179,170, 1.00), rgba(0,49,133, 1))',
+    }
+
+    const carousel1 = {
+        color: "white",
+        width: "100%",
+        height: "400px",
+        backgroundImage: 'linear-gradient(to right, rgba(255,207,67, 1.00), rgba(255,117,0, 1))',
+    }
+
+    const cardBody = {
+        maxWidth: '100px',
+        marginRight: '20px'
+    }
+
     return (
         <Carousel>
-            <Carousel.Item>
-                <svg className="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400"
-                     xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide"
-                     preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#777"></rect>
-                    <text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
-                </svg>
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
+            <Carousel.Item style={carousel} className="d-flex align-items-center justify-content-center">
+                <div>
+                <div className="d-flex align-items-center justify-content-center">
+                    <h1>4K UHD</h1>
+                </div>
+                <div className="d-flex align-items-center justify-content-center">
+                    <Card style={cardBody}>
+                        <img src="/poster/luca.jpg" className="card-img-top" alt="..."/>
+                    </Card>
+                    <Card style={cardBody}>
+                        <img src="/poster/spirit.jpg" className="card-img-top" alt="..."/>
+                    </Card>
+                    <Card style={cardBody}>
+                        <img src="/poster/man.jpg" className="card-img-top" alt="..."/>
+                    </Card>
+                    <Card style={cardBody}>
+                        <img src="/poster/icarly.jpg" className="card-img-top" alt="..."/>
+                    </Card>
+                    <Card style={cardBody}>
+                        <img src="/poster/war.jpg" className="card-img-top" alt="..."/>
+                    </Card>
+                    <Card style={cardBody}>
+                        <img src="/poster/doctor.jpg" className="card-img-top" alt="..."/>
+                    </Card>
+                </div>
+                <div className="d-flex align-items-center justify-content-center">
+                    <h3>3 Films for $14.99</h3>
+                </div>
+                </div>
             </Carousel.Item>
-            <Carousel.Item>
-                <svg className="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400"
-                     xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide"
-                     preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#777"></rect>
-                    <text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
-                </svg>
-
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <svg className="bd-placeholder-img bd-placeholder-img-lg d-block w-100" width="800" height="400"
-                     xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: First slide"
-                     preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#777"></rect>
-                    <text x="50%" y="50%" fill="#555" dy=".3em">First slide</text>
-                </svg>
-
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
+            <Carousel.Item style={carousel1} className="d-flex align-items-center justify-content-center">
+                <div>
+                    <div className="d-flex align-items-center justify-content-center">
+                        <h1>ANIMATION SALE</h1>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-center">
+                        <Card style={cardBody}>
+                            <img src="/poster/spirit.jpg" className="card-img-top" alt="..."/>
+                        </Card>
+                        <Card style={cardBody}>
+                            <img src="/poster/record.jpg" className="card-img-top" alt="..."/>
+                        </Card>
+                        <Card style={cardBody}>
+                            <img src="/poster/luca.jpg" className="card-img-top" alt="..."/>
+                        </Card>
+                        <Card style={cardBody}>
+                            <img src="/poster/boss.jpg" className="card-img-top" alt="..."/>
+                        </Card>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-center">
+                        <h3>25% OFF Movies & TV Shows</h3>
+                    </div>
+                </div>
             </Carousel.Item>
         </Carousel>
     )
