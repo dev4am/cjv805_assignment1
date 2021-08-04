85c93d990c688f9ee9370ec374f5fb5b632ff579
zfkhrl@gmail.com <zfkhrl@gmail.com>
2021-08-04 15:21:34 -0400

    profile

--- src/BackendUrl.js
+++ src/BackendUrl.js
@@ -10,13 +10,13 @@ const dev = {
 }
 
 const prod = {
-    REGISTER: "http://localhost:8080/user/register",
-    LOGIN: "http://localhost:8080/user/login",
-    FEATURED_MOVIE: "https://cjv805-fengkuizhang-db.herokuapp.com/featuredMovieList",
-    FEATURED_TV: "https://cjv805-fengkuizhang-db.herokuapp.com/featuredTvList",
-    ALL_MOVIE: "https://cjv805-fengkuizhang-db.herokuapp.com/movieList",
-    ALL_TV: "https://cjv805-fengkuizhang-db.herokuapp.com/tvList",
-    VIDEO_DETAIL: "https://cjv805-fengkuizhang-db.herokuapp.com/video",
+    REGISTER: "https://cjv805-fengkuizhang-springboot.herokuapp.com/user/register",
+    LOGIN: "https://cjv805-fengkuizhang-springboot.herokuapp.com/user/login",
+    FEATURED_MOVIE: "https://cjv805-fengkuizhang-springboot.herokuapp.com/video/featured/movie",
+    FEATURED_TV: "https://cjv805-fengkuizhang-springboot.herokuapp.com/video/featured/tv",
+    ALL_MOVIE: "https://cjv805-fengkuizhang-springboot.herokuapp.com/video/movie",
+    ALL_TV: "https://cjv805-fengkuizhang-springboot.herokuapp.com/video/tv",
+    VIDEO_DETAIL: "https://cjv805-fengkuizhang-springboot.herokuapp.com/video",
 }
 
-export const BackendUrl = dev;
+export const BackendUrl = process.env.ACTIVE_PROFILE || dev;
